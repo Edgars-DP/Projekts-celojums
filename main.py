6898b3577fe23c1debd78e91013992e3aeba1792
Edgars Polis <01dtepolis@rvt.lv>
2021-11-04 21:47:35 +0200

    Apceļošanas lapa

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+valstis = [
+    ["Krētas sala", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+    ["Spānija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+    ["Grieķija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+    ["Bulgārija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+]
+
 @app.route("/")
 def splashpage():
     return render_template('splash.html')
+
+@app.route("/piedzivojums")
+def homepage():
+    return render_template('piedzivojums.html', valstis=valstis)
