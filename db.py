f2fd810880929a6567b2ee5b19dbf1bd87029e98
Edgars Polis <01dtepolis@rvt.lv>
2021-11-05 16:14:29 +0200

    Pareiza DB inicializācija

--- db.py
+++ db.py
@@ -9,19 +9,21 @@ tr = con.cursor()
 # 2. Izveido noklusējuma datus
 
 # Valstis piedzīvijumu ekarānā
-tr.execute("DROP TABLE IF EXISTS valstis;")
 tr.execute("""
 CREATE TABLE IF NOT EXISTS valstis (vards text, apraksts text, bilde text);
 """)
-tr.execute("""
-INSERT INTO valstis (vards, apraksts, bilde) 
-	VALUES 
-		("valsts1", "apraksts1 Lorem ipsum", "bilde1"),
-		("valsts2", "apraksts2 Lorem ipsum", "bilde2"),
-		("valsts3", "apraksts3 Lorem ipsum", "bilde3"),
-		("valsts4", "apraksts4 Lorem ipsum", "bilde4"),
-		("valsts5", "apraksts5 Lorem ipsum", "bilde5")
-""")
+
+if(len(tr.execute("SELECT * FROM valstis").fetchall()) == 0):
+	# Noklusējuma vērtības ja pirmo reizi izveido
+	tr.execute("""
+		INSERT INTO valstis (vards, apraksts, bilde) 
+			VALUES 
+				("valsts1", "apraksts1 Lorem ipsum", "bilde1"),
+				("valsts2", "apraksts2 Lorem ipsum", "bilde2"),
+				("valsts3", "apraksts3 Lorem ipsum", "bilde3"),
+				("valsts4", "apraksts4 Lorem ipsum", "bilde4"),
+				("valsts5", "apraksts5 Lorem ipsum", "bilde5")
+	""")
 
 # Administratora pieejas
 tr.execute("""
