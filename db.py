87f805f1822153fd4f2d7a1c065df199d9bdaab7
Edgars Polis <01dtepolis@rvt.lv>
2021-11-05 11:39:40 +0200

    Izmaiņas

--- db.py
+++ db.py
@@ -8,20 +8,27 @@ tr = con.cursor()
 
 # 2. Izveido noklusējuma datus
 
-tr.execute("DROP TABLE valstis;")
-
+# Valstis piedzīvijumu ekarānā
+tr.execute("DROP TABLE IF EXISTS valstis;")
 tr.execute("""
 CREATE TABLE IF NOT EXISTS valstis (vards text, apraksts text, bilde text);
 """)
-
 tr.execute("""
-INSERT OR IGNORE INTO valstis (vards, apraksts, bilde) 
+INSERT INTO valstis (vards, apraksts, bilde) 
 	VALUES 
 		("valsts1", "apraksts1 Lorem ipsum", "bilde1"),
 		("valsts2", "apraksts2 Lorem ipsum", "bilde2"),
 		("valsts3", "apraksts3 Lorem ipsum", "bilde3"),
 		("valsts4", "apraksts4 Lorem ipsum", "bilde4"),
 		("valsts5", "apraksts5 Lorem ipsum", "bilde5")
+""")
+
+# Administratora pieejas
+tr.execute("""
+DROP TABLE IF EXISTS admins;
+""")
+tr.execute("""
+CREATE TABLE admins (sesija text);
 """)
 
 con.commit()
