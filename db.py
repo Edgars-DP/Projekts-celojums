c20c23d86b49b257bfc4723f6e4f067df60ee812
Edgars Polis <01dtepolis@rvt.lv>
2021-11-05 11:32:04 +0200

    Sāku darbu ar datubāzi

--- db.py
+++ db.py
@@ -1,2 +1,30 @@
-# Datubāze
+import sqlite3
+# Datubāzes savienojums un manaģēšana.
+# Importē šo failu lai izmantotu
 
+# 1. izveido savienojumu ar lokālu failu
+con = sqlite3.connect('data.db')
+tr = con.cursor()
+
+# 2. Izveido noklusējuma datus
+
+tr.execute("DROP TABLE valstis;")
+
+tr.execute("""
+CREATE TABLE IF NOT EXISTS valstis (vards text, apraksts text, bilde text);
+""")
+
+tr.execute("""
+INSERT OR IGNORE INTO valstis (vards, apraksts, bilde) 
+	VALUES 
+		("valsts1", "apraksts1 Lorem ipsum", "bilde1"),
+		("valsts2", "apraksts2 Lorem ipsum", "bilde2"),
+		("valsts3", "apraksts3 Lorem ipsum", "bilde3"),
+		("valsts4", "apraksts4 Lorem ipsum", "bilde4"),
+		("valsts5", "apraksts5 Lorem ipsum", "bilde5")
+""")
+
+con.commit()
+
+def db_exec(query):
+	return tr.execute(query)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+from db import *
 
 app = Flask(__name__)
 
-valstis = [
-    ["Krētas sala", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
-    ["Spānija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
-    ["Grieķija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
-    ["Bulgārija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
-]
+#valstis = [
+#    ["Krētas sala", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+#    ["Spānija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+#    ["Grieķija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+#    ["Bulgārija", "Lorem ipsum sdfdsf sdfsdf sdfsdf sdfsdf"],
+#]
+
+print(db_exec("select * from valstis").fetchall())
 
 @app.route("/")
 def splashpage():
@@ -15,4 +18,4 @@ def splashpage():
 
 @app.route("/piedzivojums")
 def homepage():
-    return render_template('piedzivojums.html', valstis=valstis)
+    return render_template('piedzivojums.html', valstis=db_exec("select * from valstis").fetchall())
